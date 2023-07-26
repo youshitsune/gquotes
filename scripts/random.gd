e16e0eac84e30e7380833e65c6308446b091e11f
youshitsune <youshitsune@dmz.rs>
2023-07-26 12:19:09 +0200

    first testing version, with three quotes

---
+++ scripts/random.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	var Quotes: Array[String] = ["Do not shun death in the Way", "Have no luxury in your house", "Never be jealous of others, good or bad"]
+	get_node("/root/Control/Label").set_text(Quotes.pick_random())
+	
