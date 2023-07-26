713876ac322b650f2021f243e3fa156ce777e8e8
youshitsune <youshitsune@dmz.rs>
2023-07-26 13:21:19 +0200

    add more quotes

--- scripts/get_quotes.gd
+++ scripts/get_quotes.gd
@@ -3,7 +3,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var Quotes: Array[String] = ["Do not shun death in the Way", "Have no luxury in your house", "Never be jealous of others, good or bad"]
+	var Quotes: Array[String] = ["Do not shun death in the Way - Miyamoto Musashi", "Have no luxury in your house - Miyamoto Musashi", "Never be jealous of others, good or bad - Miyamoto Musashi", "Men get arrested, dogs get put down - Rorschach, 'Watchmen'", "The streets are extended gutters and the gutters are full of blood and when the drains finally scab over, all the vermin will drown. - Rorschach, 'Watchmen'", "Do not seek pleasure for its own sake - Miyamoto Musashi", "What, So Everyone’s Supposed To Sleep Every Single Night Now? - Rick Sanchez", "I Invent, Transform, Create, And Destroy For A Living. - Rick Sanchez", "Sometimes Science Is More Art Than Science. - Rick Sanchez", "Weddings Are Basically Funerals With Cake. - Rick Sanchez", "To live is to risk it all; otherwise you’re just an inert chunk of randomly assembled molecules drifting wherever the universe blows you. - Rick Sanchez", "No. Not even in the face of Armageddon. Never compromise. - Rorscach, 'Watchmen'"]
 	set_text("\n\n".join(Quotes))
 
 
